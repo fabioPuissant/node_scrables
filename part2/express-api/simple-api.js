329e5a5113f95f2831484aec03fad738e15b54f0
fabioPuissant <fabio.puissant@hotmail.com>
2020-01-24 13:34:24 +0100

    Added POST request example and th blackboard examples

--- part2/express-api/simple-api.js
+++ part2/express-api/simple-api.js
@@ -6,6 +6,16 @@ var _ = require('underscore');
 var boeken = require('./boeken.json');
 var auteurs = require('./auteurs.json');
 
+// Statische bestanden
+app.use(express.static(__dirname + '/public'));
+
+// Middleware
+app.use((req, res, next) => {
+  console.log('Requested file: ', req.url);
+  next();
+});
+
+//GET request
 app.get('/api/boeken', (req, res) => res.json(boeken));
 app.get('/api/auteurs', (req, res) => res.json(auteurs));
 app.get('/', (req, res) => {
@@ -14,13 +24,34 @@ app.get('/', (req, res) => {
   msg += '<p>Gebruik \\api\\boeken voor een lijst met boeken.</p>';
   res.send(msg);
 });
-
+// Get met parameter
 app.get('/api/boeken/:id', (req, res) => {
   var idparam = req.params.id;
   console.log(`Found the id of ${idparam} in the routing parameters.`);
 
   var found = boeken.filter(boek => boek.id == idparam);
   res.json(found);
+});
+
+// POST
+
+// Middleware laden voor het parsen van JSON in het request
+var bodyParser = require('body-parser');
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// POst reauest verwerken
+var user = {};
+app.post('/post', (req, resp) => {
+  console.log(req.body);
+  user.username = req.body.username;
+  user.email = req.body.email;
+
+  // Echo het user object terug nqqr de client...
+  resp.json(user);
 });
 
 app.listen(3000);
